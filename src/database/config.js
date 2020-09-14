c1cd772fce72da348915d8f352e2b6787316117d
leitegs <leitedasilvag@gmail.com>
2020-09-14 20:13:41 -0300

    config database in cloud

---
+++ src/database/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	dialect: "postgres",
+	dialectOptions: {
+		ssl: true
+	},
+	port: "5432",
+	host: "echo.db.elephantsql.com",
+  password: "k4US_3uo7F4JyGCh_dwsFtmsVWgSbp1j",
+  username: "qfjuzkes",
+  database: "qfjuzkes",
+	define: {
+		timestamps: true,
+		underscored: true
+	},
+	logging: false
+}
