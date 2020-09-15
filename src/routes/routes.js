bbb040287fe275c66abef3a4afffd9310d4c7a5b
leitegs <leitedasilvag@gmail.com>
2020-09-14 21:41:47 -0300

    create routes of register, login and logout

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,25 @@
 const { Router } = require('express');
 const routes = Router();
 
+//authentication
+const authentication = require('../middleware/auth')
+
 //controllers
 const Authcontroller = require('../controllers/AuthController')
+const UserController = require('../controllers/UserController')
 
 routes.get('/', (req, res) => {
   res.send('oi')
 })
 
+//register
+routes.post('/register', UserController.create)
+
 // Login and logout
 routes.post('/login', Authcontroller.authenticate);
 routes.post('/logout', Authcontroller.logoutUser);
+
+//user routes
+routes.get('/user/:id', authentication, UserController.show);
 
 module.exports = routes;
