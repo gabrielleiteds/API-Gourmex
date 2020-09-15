1008716f83e0c5b7a558208fc29d8a7657d7343e
leitegs <leitedasilvag@gmail.com>
2020-09-15 03:27:54 -0300

    create routes for upload of videos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,8 @@ const routes = Router();
 //authentication
 const authentication = require('../middleware/auth')
 
+//multer
+const multer = require('../middleware/multer')
 //controllers
 const Authcontroller = require('../controllers/AuthController')
 const UserController = require('../controllers/UserController')
@@ -21,5 +23,17 @@ routes.post('/logout', Authcontroller.logoutUser);
 
 //user routes
 routes.get('/user/:id', authentication, UserController.show);
+routes.post('/user/upload/:id', authentication, multer.single('video'), (req, res) => {
+	// Se houve sucesso no armazenamento
+    if (req.file) {
+    	const originalname = req.file.originalname
+    	console.log(originalname)
+        // Vamos imprimir na tela o objeto com os dados do arquivo armazenado
+        return res.send(req.file);
+    }
+
+    // Se o objeto req.file for undefined, ou seja, não houve sucesso, vamos imprimir um erro!
+    return res.send('Houve erro no upload!');
+})
 
 module.exports = routes;
