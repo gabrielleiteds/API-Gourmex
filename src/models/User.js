0e5ae6586f5bed3daa87fe34e0364772799dc145
leitegs <leitedasilvag@gmail.com>
2020-09-14 21:39:50 -0300

    edit models from user

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,43 @@
-const { Model, DataTypes } = require('sequelize')
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+const bcryptjs = require('bcryptjs');
+const uuid = require('uuid');
 
-class User extends Model {
-  static init(connection) {
-    super.init(
-      {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-      },
-      {
-        sequelize: connection,
-      }
-    );
-  }
+const ROUNDS = 10;
+
+
+const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+}, {
+  hooks: {
+    afterValidate: async (user) => {
+      // generate uuid v4:
+      user.id = uuid.v4();
+
+      // encrypt the password:
+      if (user.password)
+        user.password = await bcryptjs.hash(user.password, ROUNDS);
+    }
+  },
+  sequelize,
+  tableName: 'users',
+  underscored: true
+});
+
+User.prototype.comparePassword = function (password) {
+  return bcryptjs.compare(password, this.password);
 }
 
 module.exports = User;
