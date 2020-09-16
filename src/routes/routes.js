6b1094462048667ebd658cf6ee01ca61299490c1
leitegs <leitedasilvag@gmail.com>
2020-09-15 22:39:44 -0300

    edit routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,27 @@
-const { Router } = require('express');
-const routes = Router();
+const express = require('express');
+const routes = express();
 
 //authentication
 const authentication = require('../middleware/auth')
 
 //multer
 const multer = require('../middleware/multer')
+
+routes.use(express.static("public"));
+
+//nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    autoescape: true,
+    express: routes
+});
+
 //controllers
 const Authcontroller = require('../controllers/AuthController')
 const UserController = require('../controllers/UserController')
 const UploadController = require('../controllers/UploadController')
 routes.get('/', (req, res) => {
-  res.send('oi')
+  res.render('index.html')
 })
 
 //register
