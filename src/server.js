e3d70b9338fb0118af16202b90e4e1ece78c9f17
leitegs <leitedasilvag@gmail.com>
2020-09-15 22:42:09 -0300

    add bodyparser

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
 const server = express()
 
@@ -8,8 +10,14 @@ const routes = require('./routes/routes')
 //connection with database
 require("./database/connection");
 
-server.use(express.json())
-server.use(cors({ origin: 'http://localhost:8000' }))
+server.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+server.use(bodyParser.text({ defaultCharset: 'utf-8' }));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 server.use(routes)
 
 const PORT = process.env.PORT || 8000;
